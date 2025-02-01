984931c29b4a0e6109f4d8e65abddc3a8a09339e
seohyunwoo-0407 <shw4166@kaist.ac.kr>
2025-02-01 14:06:19 +0900

    Initial commit 2

---
+++ src/main.py
@@ -0,0 +1,8 @@
+def main():
+    """
+    메인 함수
+    """
+    print("GAR Project Started")
+
+if __name__ == "__main__":
+    main() 
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+def test_sample():
+    """
+    샘플 테스트 함수
+    """
+    assert True 
